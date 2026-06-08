5b92b936e2a13cab207fe6f5ddf85cfeee7db154
Bethsabet <bethsa1996@gmail.com>
2026-06-08 16:31:03 +0100

    Primer commit - estructura inicial spring boot

---
+++ src/main/java/com/beth/gusamir/GusamirSieteSendasApplication.java
@@ -0,0 +1,13 @@
+package com.beth.gusamir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GusamirSieteSendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GusamirSieteSendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beth/gusamir/GusamirSieteSendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beth.gusamir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GusamirSieteSendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
